cea21dcdb566b949fe3e8a96bd6718b3179942f5
Yash Bhalodi <yashbhalodi@toddleapp.com>
2022-06-04 18:19:18 +0530

    fix: minor string util refactor

--- site/utils.js
+++ site/utils.js
@@ -1,13 +1,25 @@
 export const secondsToDhms = (seconds) => {
   seconds = Number(seconds);
+
+  const y = Math.floor(seconds / 31536000);
   const d = Math.floor(seconds / (3600 * 24));
   const h = Math.floor((seconds % (3600 * 24)) / 3600);
   const m = Math.floor((seconds % 3600) / 60);
   const s = Math.floor(seconds % 60);
 
-  const dDisplay = d > 0 ? d + (d == 1 ? " day, " : " days, ") : "";
-  const hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " hours, ") : "";
-  const mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " minutes, ") : "";
-  const sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : "";
-  return dDisplay + hDisplay + mDisplay + sDisplay;
+  const durationComponents = [
+    { label: y === 1 ? "year" : "years", count: y },
+    { label: d === 1 ? "day" : "days", count: d },
+    { label: h === 1 ? "hour" : "hours", count: h },
+    { label: m === 1 ? "minute" : "minutes", count: m },
+    { label: s === 1 ? "second" : "seconds", count: s },
+  ];
+
+  const stringDisplay = durationComponents
+    .filter((item) => item.count > 0)
+    .splice(0, 2)
+    .map((item) => `${item.count} ${item.label}`)
+    .join(", ");
+
+  return stringDisplay;
 };
