9b1f9a9dd640b123c730b1e312af70addde3f589
Yash Bhalodi <yashbhalodi@toddleapp.com>
2022-06-05 10:00:23 +0530

    fix: animated count transition

--- site/trackCount.js
+++ site/trackCount.js
@@ -2,13 +2,27 @@ import { cardsGeneratedDocRef } from "../plugin/firebase/setup";
 import { onSnapshot } from "firebase/firestore";
 import { secondsToDhms } from "./utils";
 
-const countElement = document.getElementById("generated-cards-count");
-const durationSavedElement = document.getElementById("time-saved-duration");
+const updateCountNode = (latestCount) => {
+  const countElement = document.getElementById("generated-cards-count");
+  const clonedCountElement = countElement.cloneNode(true);
+  clonedCountElement.innerText = latestCount;
+  countElement.parentElement.replaceChild(clonedCountElement, countElement);
+};
+
+const updateTimeSavedNode = (latestDurationString) => {
+  const durationSavedElement = document.getElementById("time-saved-duration");
+  const clonedDurationSavedElement = durationSavedElement.cloneNode(true);
+  clonedDurationSavedElement.innerText = latestDurationString;
+  durationSavedElement.parentElement.replaceChild(
+    clonedDurationSavedElement,
+    durationSavedElement
+  );
+};
 
 const unsubscribe = onSnapshot(cardsGeneratedDocRef, (doc) => {
   const { count = 0 } = doc.data();
-  countElement.innerText = count;
-  durationSavedElement.innerText = secondsToDhms(count * 3);
+  updateCountNode(count);
+  updateTimeSavedNode(secondsToDhms(count * 3));
 });
 
 window.onunload = () => {
