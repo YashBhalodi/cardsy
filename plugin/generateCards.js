4de4723b5d97be85800e4eede697612ba113ff2d
Vedran <vedran@hookshot.ca>
2023-06-16 21:50:12 -0700

    Add support for mind maps

--- plugin/generateCards.js
+++ plugin/generateCards.js
@@ -3,19 +3,26 @@ import { logEvent } from "./firebase/utils";
 const { board } = window.miro;
 
 const generateCardObjectFor = (object, x, y) => {
-  const objectFillColor = object.style.fillColor;
 
   let cardColor = "#2399f3";
-  if (objectFillColor !== "transparent") {
-    if (COLOR_MAP[objectFillColor]) {
-      cardColor = COLOR_MAP[objectFillColor];
-    } else {
-      cardColor = objectFillColor;
+  
+  if(object?.style?.fillColor) {
+    const objectFillColor = object.style.fillColor;
+
+    if (objectFillColor !== "transparent") {
+      if (COLOR_MAP[objectFillColor]) {
+        cardColor = COLOR_MAP[objectFillColor];
+      } else {
+        cardColor = objectFillColor;
+      }
     }
   }
 
+  // Add support for mind maps with text
+  let title = object?.content || object?.nodeView?.content;
+
   const cardObject = {
-    title: object.content,
+    title: title,
     x: x,
     y: y,
     style: {
@@ -33,7 +40,7 @@ export const generateCards = async () => {
 
   // filtering out shapes from all the selected widgets.
   selectedWidgets = selectedWidgets.filter((item) => {
-    return ["shape", "text", "sticky_note"].includes(item.type);
+    return ["shape", "text", "sticky_note", "mindmap_node"].includes(item.type);
   });
 
   const cardsObjects = selectedWidgets.map((item) =>
